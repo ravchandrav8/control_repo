b0c386f05907909cd966c37d8028c8df4f4945e7
ravchandrav8 <80932445+ravchandrav8@users.noreply.github.com>
2021-03-26 11:54:16 -0400

    Update init.pp

--- modules/devenv/manifests/init.pp
+++ modules/devenv/manifests/init.pp
@@ -3,4 +3,11 @@ file {'/root/tesclass.txt':
       ensure=>file,
       content=>'this is extra text',
       }
+file {'/root/RaviFile.txt':
+      ensure=>absent,
+      }
+file {'/root/tesclass2.txt':
+      ensure=>present,
+      content=>'extra lines',
+      }
       }
